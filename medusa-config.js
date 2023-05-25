141d4c6532f7f741769aae841cdc3e90219cc01e
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-05-25 06:17:37 +0300

    Algolia Search plugin added

--- medusa-config.js
+++ medusa-config.js
@@ -44,6 +44,37 @@ const plugins = [
       path: "/"
     },
   },
+  {
+    resolve: `medusa-plugin-algolia`,
+    options: {
+      applicationId: process.env.ALGOLIA_APP_ID,
+      adminApiKey: process.env.ALGOLIA_ADMIN_API_KEY,
+      settings: {
+        products: {
+          indexSettings: {
+            searchableAttributes: ["title", "description"],
+            attributesToRetrieve: [
+              "id",
+              "title",
+              "description",
+              "handle",
+              "thumbnail",
+              "variants",
+              "variant_sku",
+              "options",
+              "collection_title",
+              "collection_handle",
+              "images",
+            ],
+          },
+          transform: (product) => ({ 
+            id: product.id,
+            // other attributes...
+          }),
+        },
+      },
+    },
+  },
 ];
 
 const modules = {
